b5566ee29ea70e981cbd7ea74f43352789afeeff
Omid Vatandoost <omidvatan94@gmail.com>
2018-10-03 21:23:43 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,7 +2,7 @@
 
 def run_guessing_game
   puts "Guess a number between 1 and 6.\n"
-  number = rand(6)
+  number = rand(1..6).to_s
 
   input = gets.chomp
   
@@ -14,7 +14,7 @@ def run_guessing_game
     end
 
     puts "Guess a number between 1 and 6.\n"
-    number = rand(6)
+    number = rand(1..6).to_s
     input = gets.chomp
   end
 
