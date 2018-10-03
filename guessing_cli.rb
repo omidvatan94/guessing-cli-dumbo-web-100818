8754fe681a1c8a5873e424b6e3d43a18dd4e1c9e
Omid Vatandoost <omidvatan94@gmail.com>
2018-10-03 21:24:07 +0000

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,7 +1,7 @@
 # Code your solution here!
 
 def run_guessing_game
-  puts "Guess a number between 1 and 6.\n"
+  puts "Guess a number between 1 and 6."
   number = rand(1..6).to_s
 
   input = gets.chomp
@@ -13,7 +13,7 @@ def run_guessing_game
       puts "The computer guessed #{number}."
     end
 
-    puts "Guess a number between 1 and 6.\n"
+    puts "Guess a number between 1 and 6."
     number = rand(1..6).to_s
     input = gets.chomp
   end
